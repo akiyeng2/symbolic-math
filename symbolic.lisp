8f7201ab6b627698173c6f7893a970d289ee2673
Charles Zhang <charleszhang99@yahoo.com>
2014-03-11 19:32:18 -0400

    Revert previous commit

--- symbolic.lisp
+++ symbolic.lisp
@@ -13,15 +13,13 @@
 	 ,@clauses
 	 (t `(,',operator ,a))))
 
-(defmacro simple-infix (a b operator clauses-one clauses-two clauses-three)
+(defmacro simple-infix (a b operator clauses-one clauses-two)
   `(cond ((and (numberp ,a) (numberp ,b))
 	  (,operator ,a ,b))
 	 ((numberp ,a)
 	  (cond ,@clauses-one))
 	 ((numberp ,b)
 	  (cond ,@clauses-two))
-	 ((eql a b)
-	  (cond ,@clauses-three)) 
 	 (t `(,a ,',operator ,b))))
 
 ;;; Simplifying functions
